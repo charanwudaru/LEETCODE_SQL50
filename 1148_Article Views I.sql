51fbc7490aebb0f97917d5c8f3b493684daa9f04
Charan Sai Reddy W <72861534+charanwudaru@users.noreply.github.com>
2024-09-11 21:32:33 +0530

    1148_Article Views I.sql

--- 1148_Article Views I.sql
+++ 1148_Article Views I.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
-select authoe_id AS id from Veiws
-where author_id=viewer_id
+select distinct author_id as id from Views
+where author_id = viewer_id 
 order by id;
